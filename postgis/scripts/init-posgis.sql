d017868ccca2a082c1bdf068d8ddff17bef84925
0-don <don.cryptus@gmail.com>
2025-09-23 00:44:57 +0200

    update init-posgis.sql to install PostGIS in template1 and main database

--- postgis/scripts/init-posgis.sql
+++ postgis/scripts/init-posgis.sql
@@ -1,3 +1,9 @@
--- Enable PostGIS extension globally
-CREATE EXTENSION IF NOT EXISTS postgis;
-CREATE EXTENSION IF NOT EXISTS postgis_topology;
+-- Install in template1 so all new databases get PostGIS
+   \c template1
+   CREATE EXTENSION IF NOT EXISTS postgis;
+   CREATE EXTENSION IF NOT EXISTS postgis_topology;
+   
+   -- Also install in your main database
+   \c bussiness-finder
+   CREATE EXTENSION IF NOT EXISTS postgis;
+   CREATE EXTENSION IF NOT EXISTS postgis_topology;
